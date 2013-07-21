ea32f1ca66dff63a8259b8f32031a477d9ab1aaf
arachnegl <gregoryloyse@gmail.com>
2013-07-21 20:23:34 +0100

    new config fro prelude

---
+++ personal.el
@@ -0,0 +1,22 @@
+(disable-theme 'zenburn)
+
+;; (setq prelude-whitespace nil)
+
+;; (setq prelude-flyspell nil)
+
+;; PYTHON
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; (add-hook 'after-init-hook                             ;; ;;
+;;           #'(lambda ()                                 ;; ;;
+;;               (setq jedi:setup-keys t)                 ;; ;;
+;;               (add-hook 'python-mode-hook 'jedi:setup) ;; ;;
+;;               (setq jedi:complete-on-dot t)))          ;; ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+(provide 'personal)
+;;; personal ends here
